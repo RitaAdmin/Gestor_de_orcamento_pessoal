277d4a8a834d0255febfa17841be9ec87f1c4545
RitaAdmin <uxiuexercicios@gmail.com>
2025-07-19 15:55:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from tkinter import *
 from tkinter import Tk, ttk
 
+# Definicao das cores da interface (cores do colorpicker)
+co0="#1b1b1c" #preto
+co1="#f7f7fa" #branco
+co2="#3a86ff" # Azul 
+co3="#ffbe0b" # Amarelo
+co4="#fa056f" # Rosa
+co5="#8338ec" # Roxo
+co6="#fb5607" # Laranja 
+co7="#219ebc" # Azul claro
+co8="#8ecae6" # Azul pastel
+co9="#ea70a5" # Rosa claro
+
+colours=["#2e2e2f",  "#d9dbdf", "#265ea6","#e0a800", "#b33a5a", "#6829b0","#d15406",  "#1b7287", "#7bb2d9", "#c96192"]
+
 
