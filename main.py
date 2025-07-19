3eabd2e8ad0ec8c157e442acc94996dc9d4ca496
RitaAdmin <uxiuexercicios@gmail.com>
2025-07-19 17:41:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,38 @@ co9="#ea70a5" # Rosa claro
 
 colours=["#2e2e2f",  "#d9dbdf", "#265ea6","#e0a800", "#b33a5a", "#6829b0","#d15406",  "#1b7287", "#7bb2d9", "#c96192"]
 
+#criar janela do interface
+janela=Tk()
+janela.title("Gestor de finaças pessoais")
+janela.geometry('1100x750')
+janela.configure(background="#d9dbdf")
+janela.resizable(width=False, height=False)
 
+style=ttk.Style(janela)
+style.theme_use("clam") # escolhi clam por ser o mais versatil de todos
+
+#criar frames da janela---------------------------------------------------------------------------------------------
+#titulo
+frameCima = Frame(janela, width=1100, height=50, bg=co1, relief="flat")
+frameCima.grid(row=0, column=0)
+
+#Meio - Inserir dados
+frameMeio = Frame(janela, width=1100, height=200, bg=co1, pady=20, relief="raised")
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+#baixo_filtros ?
+
+frameBaixo = Frame(janela, width=1100, height=500, bg=co1, relief="raised")
+frameBaixo.grid(row=2, column=0, pady=1, padx=0, sticky=NSEW)
+
+
+
+
+
+
+
+
+
+
+
+janela.mainloop()
